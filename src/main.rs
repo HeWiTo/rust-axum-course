d3fdbec8eab06b43cba8a9d029f1a812e36311ef
Henri <h.w.tompodung@gmail.com>
2023-11-01 22:24:56 +0700

    01-hello-world

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{
+    Router, 
+    response::Html,
+    routing::get,
+};
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello", 
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+
+    // region: --- Start server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap()
+    // endregion: --- Start Server
+}
