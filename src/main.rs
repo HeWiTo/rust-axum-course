5eead4cc18178906d2bf471e2d1e535ac2046c11
Henri <h.w.tompodung@gmail.com>
2023-12-24 18:14:16 +0700

    04-DB-live-reload

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod error;
 mod log;
 mod model;
 mod web;
+// #[cfg(test)] // Commented during early development
+pub mod _dev_utils;
 
 pub use self::error::{Error, Result};
 pub use config::config; // use crate::config
@@ -30,29 +32,32 @@ async fn main() -> Result<()> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-	// Initialize ModelManager.
-	let mm = ModelManager::new().await?;
+    // -- FOR DEV ONLY
+    _dev_utils::init_dev().await;
 
-	// -- Define Routes
-	// let routes_rpc = rpc::routes(mm.clone())
-	//   .route_layer(middleware::from_fn(mw_ctx_require));
+    // Initialize ModelManager.
+    let mm = ModelManager::new().await?;
 
-	let routes_all = Router::new()
-		.merge(routes_login::routes())
-		// .nest("/api", routes_rpc)
-		.layer(middleware::map_response(mw_reponse_map))
-		.layer(middleware::from_fn_with_state(mm.clone(), mw_ctx_resolve))
-		.layer(CookieManagerLayer::new())
-		.fallback_service(routes_static::serve_dir());
+    // -- Define Routes
+    // let routes_rpc = rpc::routes(mm.clone())
+    //   .route_layer(middleware::from_fn(mw_ctx_require));
 
-	// region:    --- Start Server
-	let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-	info!("->> {:<12} - {addr}\n", "LISTENING");
-	axum::Server::bind(&addr)
-		.serve(routes_all.into_make_service())
-		.await
-		.unwrap();
-	// endregion: --- Start Server
+    let routes_all = Router::new()
+        .merge(routes_login::routes())
+		    // .nest("/api", routes_rpc)
+		    .layer(middleware::map_response(mw_reponse_map))
+		    .layer(middleware::from_fn_with_state(mm.clone(), mw_ctx_resolve))
+		    .layer(CookieManagerLayer::new())
+		    .fallback_service(routes_static::serve_dir());
 
-	Ok(())
+	  // region:    --- Start Server
+	  let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+	  info!("->> {:<12} - {addr}\n", "LISTENING");
+	  axum::Server::bind(&addr)
+        .serve(routes_all.into_make_service())
+		    .await
+        .unwrap();
+    // endregion: --- Start Server
+
+    Ok(())
 }
